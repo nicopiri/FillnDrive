07f080aeffb8e1fa351040d1c1039b184e08cec8
Federico <100353451+fediiiiiiiii@users.noreply.github.com>
2023-10-26 12:11:43 +0200

    SQLite db class

--- progetto/app/src/main/java/com/example/fillndrive/DBHelper.java
+++ progetto/app/src/main/java/com/example/fillndrive/DBHelper.java
@@ -8,63 +8,91 @@ import android.database.sqlite.SQLiteOpenHelper;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class DBHelper extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "Distributori";
-    SQLiteDatabase db;
+    private static final String stazioniUrl = "https://www.mimit.gov.it/images/exportCSV/anagrafica_impianti_attivi.csv";
+    private static final String carburantiUrl = "https://www.mimit.gov.it/images/exportCSV/prezzo_alle_8.csv";
+
     public DBHelper(Context context) {
         super(context, DB_NAME, null, 1);
-        db = this.getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        System.out.println("onCreate avviata");
         db.execSQL("CREATE TABLE IF NOT EXISTS stazioni(idImpianto INTEGER PRIMARY KEY, Bandiera TEXT, Tipo_Impianto TEXT, Latitudine REAL, Longitudine REAL)");
-        db.execSQL("CREATE TABLE IF NOT EXISTS carburanti" +
-                "(idImpianto INTEGER, descCarburante TEXT, Prezzo REAL, isSelf INTEGER)");
+        db.execSQL("CREATE TABLE IF NOT EXISTS carburanti(idImpianto INTEGER, descCarburante TEXT, Prezzo REAL, isSelf INTEGER)");
     }
 
+    public void updateData() throws IOException, InterruptedException {
+        SQLiteDatabase db = this.getWritableDatabase();
 
-    public void addData(String csvUrl, String tableName) throws IOException {
-        URL cu = new URL(csvUrl);
-        BufferedReader in = new BufferedReader(new InputStreamReader(cu.openStream()));
-        List<String> lines = in.lines().collect(Collectors.toList());
-        in.close();
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        executor.submit(() -> updateTable(db, stazioniUrl, "stazioni", 10));
+        executor.submit(() -> updateTable(db, carburantiUrl, "carburanti", 5));
 
-        if(lines.size() > 0) {
-            lines.remove(0);
-            lines.remove(1);
-        }
+        executor.shutdown();
+        db.close();
+    }
 
+    private void updateTable(SQLiteDatabase db, String csvUrl, String tableName, int expectedCol) {
+        try {
+            final URL cu = new URL(csvUrl);
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(cu.openStream()))) {
+                String line;
+                int i = 0;
 
-        db.beginTransaction(); // Inizia una transazione per migliorare le prestazioni
-        db.execSQL("DELETE FROM stazioni");
-        db.execSQL("DELETE FROM carburanti");
-        for (String line : lines) {
-            String[] data = line.split(";");
-            if (data.length != 10) {
-                continue; // Salta questa riga e passa alla successiva
+                db.beginTransaction();
+
+                String deleteQuery = "DELETE FROM " + tableName;
+                db.execSQL(deleteQuery);
+
+                while ((line = in.readLine()) != null) {
+                    if (i >= 2) {
+                        String[] data = line.split(";");
+                        if(data.length == expectedCol)
+                            insert(db, tableName, data);
+                    }
+                    i++;
+                }
+                in.close();
+                db.setTransactionSuccessful();
+            } finally {
+                db.endTransaction();
             }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void insert(SQLiteDatabase db, String tableName, String[] data) {
+        if(tableName.equals("stazioni")){
             ContentValues cv = new ContentValues();
             cv.put("idImpianto", data[0]);
             cv.put("Bandiera", data[2]);
             cv.put("Tipo_Impianto", data[3]);
             cv.put("Latitudine", data[8]);
             cv.put("Longitudine", data[9]);
-
             db.insert("stazioni", null, cv);
+        }else if(tableName.equals("carburanti")){
+            ContentValues cv = new ContentValues();
+            cv.put("idImpianto", data[0]);
+            cv.put("descCarburante", data[1]);
+            cv.put("Prezzo", data[2]);
+            cv.put("isSelf", data[3]);
+            db.insert("carburanti", null, cv);
         }
-
-        db.endTransaction();
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
     }
+
 }
