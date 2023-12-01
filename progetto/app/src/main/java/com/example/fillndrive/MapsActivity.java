ee9e3b1297c814b7872d1c9d1c44adf2ea950634
Federico <100353451+fediiiiiiiii@users.noreply.github.com>
2023-12-01 16:04:48 +0100

    Implementazione drawRoute

--- progetto/app/src/main/java/com/example/fillndrive/MapsActivity.java
+++ progetto/app/src/main/java/com/example/fillndrive/MapsActivity.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
